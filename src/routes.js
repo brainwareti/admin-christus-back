f38072f1a53f138e27a905697c16e17842f0c7cb
Talles Barbosa <tbs.brainware@gmail.com>
2020-01-22 00:44:47 -0300

    Adição de rota raiz

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const routes = express.Router();
 
 const ClientController = require("./controllers/ClientController");
 
+routes.get("/", () => {
+  return res.send("In air!");
+});
+
 routes.get("/clients", ClientController.index);
 routes.post("/clients", ClientController.store);
 
